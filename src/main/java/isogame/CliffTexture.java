faca1af5ddb4d9c757769270df01ffdeda52e062
Callum <callum@callumscode.com>
2015-09-21 11:33:16 +0900

    remove unneeded textures

--- src/main/java/isogame/CliffTexture.java
+++ src/main/java/isogame/CliffTexture.java
@@ -6,27 +6,23 @@ import javafx.scene.paint.Paint;
 
 public class CliffTexture {
 	// a paint for every direction the slope could be going.
-	public final Paint ul;
-	public final Paint ll;
-	public final Paint lr;
-	public final Paint ur;
-	public final Paint flat;
+	private final Paint ul;
+	private final Paint ur;
+	private final Paint flat;
 
 	public CliffTexture(String urlWide, String urlNarrow) {
 		Image imgWide = new Image(urlWide, false);
 		Image imgNarrow = new Image(urlNarrow, false);
 		ul = new ImagePattern(imgNarrow, -1, 0, 2, 1, true);
-		ll = new ImagePattern(imgWide,    0, 0,   1, 1, true);
-		lr = new ImagePattern(imgWide,    0, 0,   1, 1, true);
 		ur = new ImagePattern(imgNarrow,  0, 0, 2, 1, true);
-		flat = new ImagePattern(imgWide,  0, 0,   1, 1, true);
+		flat = new ImagePattern(imgWide,  0, 0, 1, 1, true);
 	}
 
 	public Paint getTexture(SlopeType s) {
 		switch (s) {
 			case N: return ur;
-			case S: return ll;
-			case E: return lr;
+			case S: return flat;
+			case E: return flat;
 			case W: return ul;
 			case NONE: return flat;
 			default:
