fe28584539f9feab47eb7179355ed8ab968861ee
Callum <callum@callumscode.com>
2015-09-23 14:39:58 +0900

    elevation corrected collision detection

--- src/main/java/isogame/engine/Stage.java
+++ src/main/java/isogame/engine/Stage.java
@@ -8,6 +8,7 @@ import javafx.scene.transform.Affine;
 import javafx.scene.transform.NonInvertibleTransformException;
 import javafx.scene.transform.Rotate;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
 import static isogame.GlobalConstants.ELEVATION_H;
@@ -93,6 +94,108 @@ public class Stage {
 
 		return new MapPoint(
 			(int) Math.floor(t.getX() - 0.5), (int) Math.floor(t.getY() + 0.5));
+	}
+
+	/**
+	 * Determine which map tile the mouse is currently over, correcting for
+	 * elevation.
+	 * @return Null if there is no tile at the mouse position.
+	 * */
+	public MapPoint mouseTileCollision(Point2D in, CameraAngle a) {
+		MapPoint p = fromIsoCoord(in, a);
+		Iterator<Tile> it = terrain.iterateCollisionDetection(p, a);
+
+		Tile tile;
+		while (it.hasNext()) {
+			tile = it.next();
+
+			// compute a polygon representing the position and shape of the tile.
+			// Then we will do a test to see if the mouse point is inside the
+			// polygon.
+			double[] xs = new double[6];
+			double[] ys = new double[6];
+			int pts;
+
+			switch (tile.adjustSlopeForCameraAngle(a)) {
+				case NONE:
+					xs[0] = TILEW / 2; ys[0] = -2;
+					xs[1] = TILEW + 4; ys[1] = TILEH / 2;
+					xs[2] = TILEW / 2; ys[2] = TILEH + 2;
+					xs[3] = -4;        ys[3] = TILEH / 2;
+					pts = 4;
+					break;
+				case N:
+					xs[0] = -4;        ys[0] = (TILEH / 2) + 2;
+					xs[1] = TILEW / 2; ys[1] = 0 - (TILEH / 2) - 2;
+					xs[2] = TILEW + 4; ys[2] = 0;
+					xs[3] = TILEW + 4; ys[3] = (TILEH / 2) + 2;
+					xs[4] = TILEW / 2; ys[4] = TILEH + 4;
+					pts = 5;
+					break;
+				case E:
+					xs[0] = -4;        ys[0] = (TILEH / 2) + 2;
+					xs[1] = TILEW / 2; ys[1] = -2;
+					xs[2] = TILEW + 4; ys[2] = -2;
+					xs[3] = TILEW + 4; ys[3] = (TILEH / 2) + 2;
+					xs[4] = TILEW / 2; ys[4] = TILEH + 2;
+					pts = 5;
+					break;
+				case S:
+					xs[0] = -4;        ys[0] = -2;
+					xs[1] = TILEW / 2; ys[1] = -2;
+					xs[2] = TILEW + 4; ys[2] = (TILEH / 2) + 2;
+					xs[3] = TILEW / 2; ys[3] = TILEH  + 2;
+					xs[4] = -4;        ys[4] = (TILEH / 2) + 2;
+					pts = 5;
+					break;
+				case W:
+					xs[0] = -4;        ys[0] = 0;
+					xs[1] = TILEW / 2; ys[1] = 0 - (TILEH / 2) - 2;
+					xs[2] = TILEW + 4; ys[2] = (TILEH / 2) + 2;
+					xs[3] = TILEW / 2; ys[3] = TILEH + 4;
+					xs[4] = -4;        ys[4] = (TILEH / 2) + 2;
+					pts = 5;
+					break;
+				default: throw new RuntimeException(
+					"Invalid slope type. This cannot happen");
+			}
+
+			Point2D cp = correctedIsoCoord(tile.pos, a);
+			if (isPointInPolygon(xs, ys, pts, in.getX() - cp.getX(), in.getY() - cp.getY())) {
+				return tile.pos;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Determine if a point lies inside a convex polygon.
+	 * */
+	private boolean isPointInPolygon(
+		double[] xs, double[] ys, int pts, double x, double y
+	) {
+		double t = 0;
+
+		// for a convex polygon, we can determine if a point lies inside the
+		// polygon by checking it lies on the same side of each line on the
+		// perimeter of the polygon.
+		for (int i = 0; i < pts; i++) {
+			double lx0 = xs[i];
+			double ly0 = ys[i];
+			double lx1 = xs[(i + 1) % pts];
+			double ly1 = ys[(i + 1) % pts];
+
+			// the sign of this cross product determines which side the point is on.
+			double det = ((lx1 - lx0) * (y - ly0)) - ((ly1 - ly0) * (x - lx0));
+			if (det > 0 && t < 0 || det < 0 && t > 0) {
+				return false;
+			} else if (det != 0) {
+				t = det;
+			}
+		}
+
+		return true;
 	}
 
 	/**
