d83a0491b3963052938268cba7b36bea66b6ee57
Callum <callum@callumscode.com>
2015-10-03 11:13:16 +0900

    remove dead code

--- src/main/java/isogame/editor/EditorCanvas.java
+++ src/main/java/isogame/editor/EditorCanvas.java
@@ -320,58 +320,6 @@ public class EditorCanvas extends Pane {
 		this.tool = tool;
 	}
 
-	/**
-	 * Make a simple example stage for testing purposes.
-	 * */
-	private Stage exampleStage() throws CorruptDataException {
-		TerrainTexture black = new TerrainTexture("black", "/black.jpg");
-		TerrainTexture white = new TerrainTexture("white", "/white.jpg");
-		CliffTexture cliff = new CliffTexture(
-			"cliffs",
-			"/cliff_texture.png",
-			"/cliff_texture_narrow.png");
-		Tile[] data = new Tile[8 * 8];
-		for (int x = 0; x < 8; x++) {
-			for (int y = 0; y < 8; y++) {
-				TerrainTexture t;
-				if ((x + y) % 2 == 0) t = black; else t = white;
-				int elevation = 0;
-				SlopeType slope = SlopeType.NONE;
-				if (x == 0 && y == 1) slope = SlopeType.N;
-				if (x == 1 && y == 1) slope = SlopeType.N;
-				if (x == 0 && y == 0) elevation = 1;
-				if (x == 1 && y == 0) elevation = 1;
-
-				if (x == 0 && y == 6) slope = SlopeType.S;
-				if (x == 1 && y == 6) slope = SlopeType.S;
-				if (x == 0 && y == 7) elevation = 1;
-				if (x == 1 && y == 7) elevation = 1;
-
-				if (x == 6 && y == 0) slope = SlopeType.E;
-				if (x == 6 && y == 1) slope = SlopeType.E;
-				if (x == 7 && y == 0) elevation = 1;
-				if (x == 7 && y == 1) elevation = 1;
-
-				if (x == 7 && y == 6) slope = SlopeType.W;
-				if (x == 7 && y == 7) slope = SlopeType.W;
-				if (x == 6 && y == 6) elevation = 1;
-				if (x == 6 && y == 7) elevation = 1;
-
-				if (x == 4 && y == 3) slope = SlopeType.S;
-				if (x == 3 && y == 4) slope = SlopeType.E;
-				if (x == 4 && y == 5) slope = SlopeType.N;
-				if (x == 5 && y == 4) slope = SlopeType.W;
-
-				data[(y * 8) + x] = new Tile(
-					new MapPoint(x, y), elevation,
-					slope, false,
-					StartZoneType.NONE, t, cliff);
-			}
-		}
-		StageInfo terrain = new StageInfo(8, 8, data);
-		return new Stage(terrain);
-	}
-
 	private void setScrollingAnimation(
 		ContinuousAnimator scrolling, Set<KeyCode> keys
 	) {
