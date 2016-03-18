f39c7ef5140a67f901dd7e5d6adddaa9b6776d26
Callum <callum@callumscode.com>
2016-03-18 16:32:06 +0900

    start game root

---
+++ src/main/java/isogame/game/Game.java
@@ -0,0 +1,23 @@
+package isgame.game;
+
+import javafx.application.Application;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+	public static void main(final String[] arguments) {
+		Application.launch(arguments);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		StackPane root = new StackPane();
+		Scene scene = new Scene(root, 960, 540);
+
+		primaryStage.setTitle("In the Zone!");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
+
