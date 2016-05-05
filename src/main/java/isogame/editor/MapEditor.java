35aaf77c4fd6b175fa98049f49d1214672667234
Callum <callum@callumscode.com>
2016-05-05 11:15:03 +0900

    always prompt for data directory

--- src/main/java/isogame/editor/MapEditor.java
+++ src/main/java/isogame/editor/MapEditor.java
@@ -8,12 +8,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.io.PrintWriter;
  
 public class MapEditor extends Application {
 	public static void main(final String[] arguments) {
@@ -28,6 +23,7 @@ public class MapEditor extends Application {
 
 		try {
 			File dataDir = getDataDir(primaryStage);
+			if (dataDir == null) System.exit(1);
 
 			ToggleGroup toolsGroup = new ToggleGroup();
 			EditorCanvas canvas = new EditorCanvas(root, primaryStage);
@@ -56,42 +52,9 @@ public class MapEditor extends Application {
 	}
 
 	public File getDataDir(Stage primaryStage) {
-		File homeDir = new File(System.getProperty("user.home"));
-		File configDir = new File(homeDir, ".inthezone");
-		File configFile = new File(configDir, "config.txt");
-
-		File dataDir;
-		try (BufferedReader in = new BufferedReader(
-				new InputStreamReader(new FileInputStream(configFile), "UTF-8"))
-		) {
-			// try to read the data directory from the configuration file
-			dataDir = new File(in.readLine());
-		} catch (IOException e) {
-			// If that fails, ask the user for the data directory, then make a new
-			// configuration file.
-			final DirectoryChooser directoryChooser = new DirectoryChooser();
-			directoryChooser.setTitle("Select data directory...");
-			dataDir = directoryChooser.showDialog(primaryStage);
-
-			// Ensure the configuration directory exists
-			if (!configDir.exists()) {
-				try {
-					configDir.mkdir();
-				} catch (SecurityException e1) {
-					System.err.println("Failed to create config dir at " +
-						configDir.toString() + "\n" + e1.toString());
-				}
-			}
-
-			// Make the configuration file
-			try (PrintWriter out = new PrintWriter(configFile, "UTF-8")) {
-				out.println(dataDir.toString());
-			} catch (IOException e1) {
-				System.err.println("Cannot write config file at " +
-					configFile.toString() + "\n" + e1.toString());
-			}
-		}
-
+		final DirectoryChooser directoryChooser = new DirectoryChooser();
+		directoryChooser.setTitle("Select data directory...");
+		File dataDir = directoryChooser.showDialog(primaryStage);
 		return dataDir;
 	}
 }
