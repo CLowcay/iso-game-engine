51ad87804448fe41eccb18c7935327bf0b1c4149
Callum Lowcay <callum@callumscode.com>
2016-08-18 16:11:17 +0900

    opposite method for SlopeType

--- src/main/java/isogame/engine/SlopeType.java
+++ src/main/java/isogame/engine/SlopeType.java
@@ -1,6 +1,17 @@
 package isogame.engine;
 
 public enum SlopeType {
-	N, S, E, W, NONE
+	N, S, E, W, NONE;
+
+	public SlopeType opposite() {
+		switch(this) {
+			case N: return S;
+			case S: return N;
+			case E: return W;
+			case W: return E;
+			case NONE: return NONE;
+			default: throw new RuntimeException("This cannot happen");
+		}
+	}
 }
 
