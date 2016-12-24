ce1ba815ecbd894379e1f1c9055174e7712a76a1
Callum <callum@callumscode.com>
2016-12-24 16:15:11 +1300

    add GPL license

--- src/main/java/isogame/resource/ResourceLocator.java
+++ src/main/java/isogame/resource/ResourceLocator.java
@@ -1,3 +1,21 @@
+/* © Callum Lowcay 2015, 2016
+
+This file is part of iso-game-engine.
+
+iso-game-engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+iso-game-engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with iso-game-engine.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 package isogame.resource;
 
 import java.io.InputStream;
