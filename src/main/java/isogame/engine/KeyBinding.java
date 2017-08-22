7595dda28585e3defb586f7a60afcb50c22f3b34
Callum Lowcay <callum@callumscode.com>
2017-08-22 15:25:25 +1200

    add serialization code for keybindings

--- src/main/java/isogame/engine/KeyBinding.java
+++ src/main/java/isogame/engine/KeyBinding.java
@@ -1,18 +1,51 @@
 package isogame.engine;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * A KeyBinding is a token that represents an action that we might want to bind
  * a key to.
  * */
 public class KeyBinding {
 	// Keybindings for moving the view around
-	public final static KeyBinding scrollUp = new KeyBinding();
-	public final static KeyBinding scrollDown = new KeyBinding();
-	public final static KeyBinding scrollLeft = new KeyBinding();
-	public final static KeyBinding scrollRight = new KeyBinding();
-	public final static KeyBinding rotateLeft = new KeyBinding();
-	public final static KeyBinding rotateRight = new KeyBinding();
+	public final static KeyBinding scrollUp = new KeyBinding("scrollUp");
+	public final static KeyBinding scrollDown = new KeyBinding("scrollDown");
+	public final static KeyBinding scrollLeft = new KeyBinding("scrollLeft");
+	public final static KeyBinding scrollRight = new KeyBinding("scrollRight");
+	public final static KeyBinding rotateLeft = new KeyBinding("rotateLeft");
+	public final static KeyBinding rotateRight = new KeyBinding("rotateRight");
 
-	protected KeyBinding() { super(); }
+	private final String name;
+
+	protected KeyBinding(final String name) {
+		this.name = name;
+	}
+
+	public static Set<KeyBinding> allBindings() {
+		final Set<KeyBinding> r = new HashSet<>();
+		r.add(scrollUp);
+		r.add(scrollDown);
+		r.add(scrollLeft);
+		r.add(scrollRight);
+		r.add(rotateLeft);
+		r.add(rotateRight);
+		return r;
+	}
+
+	@Override public String toString() { return name; }
+
+	public static KeyBinding valueOf(final String s) {
+		switch (s) {
+			case "scrollUp": return scrollUp;
+			case "scrollDown": return scrollDown;
+			case "scrollLeft": return scrollLeft;
+			case "scrollRight": return scrollRight;
+			case "rotateLeft": return scrollLeft;
+			case "rotateRight": return scrollRight;
+			default:
+				throw new RuntimeException("Unhandled keybinding " + s);
+		}
+	}
 }
 
--- src/main/java/isogame/engine/KeyBindingTable.java
+++ src/main/java/isogame/engine/KeyBindingTable.java
@@ -1,15 +1,22 @@
 package isogame.engine;
 
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyEvent;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * A table of key bindings
  * */
-public class KeyBindingTable {
+public class KeyBindingTable implements HasJSONRepresentation {
 	public final Map<KeyCodeCombination, KeyBinding> keys = new HashMap<>();
 
 	public Optional<KeyBinding> getKeyAction(final KeyEvent e) {
@@ -17,6 +24,52 @@ public class KeyBindingTable {
 			if (k.match(e)) return Optional.of(keys.get(k));
 		}
 		return Optional.empty();
+	}
+
+	@Override public JSONObject getJSON() {
+		final JSONObject o = new JSONObject();
+		final JSONArray a = new JSONArray();
+		for (final KeyCodeCombination key : keys.keySet()) {
+			final JSONObject k = new JSONObject();
+			k.put("code", key.getCode().toString());
+			k.put("control", key.getControl().toString());
+			k.put("alt", key.getAlt().toString());
+			k.put("shift", key.getShift().toString());
+			k.put("shortcut", key.getShortcut().toString());
+			k.put("action", keys.get(key).toString());
+			a.put(k);
+		}
+		o.put("keys", a);
+		return o;
+	}
+
+	public static KeyBindingTable fromJSON(
+		final JSONObject json,
+		final Function<String, KeyBinding> getBinding
+	) {
+		final KeyBindingTable table = new KeyBindingTable();
+
+		final JSONArray a = json.optJSONArray("keys");
+
+		for (int i = 0; i < a.length(); i++) {
+			final JSONObject ko = a.getJSONObject(i);
+			final KeyCode code = KeyCode.valueOf(ko.getString("code"));
+			final KeyCombination.ModifierValue control =
+				KeyCombination.ModifierValue.valueOf(ko.getString("control"));
+			final KeyCombination.ModifierValue alt =
+				KeyCombination.ModifierValue.valueOf(ko.getString("alt"));
+			final KeyCombination.ModifierValue shift =
+				KeyCombination.ModifierValue.valueOf(ko.getString("shift"));
+			final KeyCombination.ModifierValue shortcut =
+				KeyCombination.ModifierValue.valueOf(ko.getString("shortcut"));
+
+			table.keys.put(
+				new KeyCodeCombination(code, shift, control, alt,
+					KeyCombination.ModifierValue.UP, shortcut),
+				getBinding.apply(ko.getString("action")));
+		}
+
+		return table;
 	}
 }
 
