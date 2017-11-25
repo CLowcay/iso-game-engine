3463ff0fa9ec6a5b1676553857455aa882ece553
Callum Lowcay <cwslowcay@gmail.com>
2017-11-25 17:04:49 +1300

    modularize all dependencies

---
+++ engine/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module isogame.engine {
+	exports isogame;
+	exports isogame.engine;
+	exports isogame.gui;
+	exports isogame.resource;
+
+	requires javafx.controls;
+
+	requires org.json;
+}
+
---
+++ mapeditor/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module isogame.editor {
+	exports isogame.editor;
+
+	requires java.desktop;
+	requires javafx.controls;
+	requires javafx.swing;
+
+	requires isogame.engine;
+	requires org.json;
+}
+
